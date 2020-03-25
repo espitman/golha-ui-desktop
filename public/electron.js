1b2614cf0eb77801af2f89bec5099dbe50fedbcb
Saeed Heidari <saeed.heidari86@gmail.com>
2020-03-25 11:10:45 +0430

    disable auto open devTools

--- public/electron.js
+++ public/electron.js
@@ -10,11 +10,6 @@ let mainWindow
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680})
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
-  if (isDev) {
-    // Open the DevTools.
-    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>')
-    mainWindow.webContents.openDevTools()
-  }
   mainWindow.on('closed', () => mainWindow = null)
 }
 
