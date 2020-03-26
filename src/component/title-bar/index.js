52d22c9c73269998d99e2253c0663ff4a2d0b712
Saeed Heidari <saeed.heidari86@gmail.com>
2020-03-26 23:39:19 +0430

    feat: title bar

--- src/component/title-bar/index.js
+++ src/component/title-bar/index.js
@@ -5,7 +5,7 @@ import './style.scss'
 export default class TitleBar extends React.Component {
   render() {
     return (
-      <div id="title-bar">
+      <div className="title-bar">
         <h1>رایو گل‌ها</h1>
       </div>
     )
