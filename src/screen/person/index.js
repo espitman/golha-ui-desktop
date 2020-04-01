754da41111b98709b9807b1a68b88d319393c4dd
Saeed Heidari <saeed.heidari86@gmail.com>
2020-04-01 14:41:43 +0430

    feat: track row

--- src/screen/person/index.js
+++ src/screen/person/index.js
@@ -96,7 +96,7 @@ class PersonScreen extends React.Component {
               </div>
               <div className={'box-tracks'}>
                 {tracks.map((track, i) => {
-                  return <TrackRow key={`track_${i}`} track={track} />
+                  return <TrackRow key={`track_${i}`} index={i} track={track} />
                 })}
               </div>
             </div>
