d56c326b8ee60356624c7cbc9644cfcdf8b94a0a
Saeed Heidari <saeed.heidari86@gmail.com>
2020-04-13 21:35:49 +0430

    feat: add player provider

---
+++ src/provider/player.js
@@ -0,0 +1,79 @@
+export default class PlayerProvider {
+  constructor(props) {
+    this.playerService = new props.PlayerService()
+    this.stateSetter = props.stateSetter
+    this.state = {}
+  }
+
+  setState = (states) => {
+    this.state = { ...this.state, ...states }
+    this.stateSetter(states)
+  }
+
+  play = (track) => {
+    this.playerService.play(track)
+    this.setState({
+      currentTrack: this.playerService.track,
+      showPlayer: this.playerService.visible,
+      isPlaying: this.playerService.isPlaying
+    })
+  }
+
+  pause = () => {
+    this.playerService.pause()
+    this.setState({
+      isPlaying: this.playerService.isPlaying
+    })
+  }
+
+  togglePlay = () => {
+    this.playerService.togglePlay()
+    const isPlaying = this.playerService.isPlaying
+    this.setState({
+      isPlaying
+    })
+    return isPlaying
+  }
+
+  hide = () => {
+    this.playerService.hide()
+    this.setState({
+      showPlayer: this.playerService.visible,
+      isPlaying: this.playerService.isPlaying
+    })
+  }
+
+  playNextTrack = () => {
+    const nextTrack = this.playerService.getNextTrack()
+    if (nextTrack) {
+      this.play(nextTrack)
+    } else {
+      this.pause()
+    }
+  }
+
+  clearPlayList = () => {
+    this.playerService.clearPlayList()
+  }
+
+  addToPlayList = (track) => {
+    const isInPlayList = this.playerService.addToPlayList(track)
+    if (
+      !this.playerService.isPlaying &&
+      this.playerService.playList.length === 1
+    ) {
+      this.play(track)
+    }
+    this.setState({ playlist: this.playerService.playList })
+    return isInPlayList
+  }
+
+  removeFromPlayList = (track) => {
+    this.setState({ playlist: this.playerService.playList })
+    return this.playerService.removeFromPlayList(track)
+  }
+
+  isInPlayList = (_id) => {
+    return this.playerService.isInPlayList(_id)
+  }
+}
