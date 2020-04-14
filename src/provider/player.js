f06835398852557bbc7436536ea28aeb1411d196
Saeed Heidari <saeed.heidari86@gmail.com>
2020-04-14 23:14:19 +0430

    feat: merge player service to provider

--- src/provider/player.js
+++ src/provider/player.js
@@ -1,55 +1,115 @@
+import { findIndex } from 'lodash'
+
 export default class PlayerProvider {
   constructor(props) {
-    this.playerService = new props.PlayerService()
     this.stateSetter = props.stateSetter
+    this.visible = false
+    this.isPlaying = false
+    this.track = {}
+    this.playList = []
     this.state = {}
   }
 
   setState = (states) => {
-    this.state = { ...this.state, ...states }
     this.stateSetter(states)
   }
 
+  show() {
+    this.visible = true
+  }
+  hide() {
+    this.visible = false
+  }
+
   play = (track) => {
-    const isInPlayList = this.playerService.isInPlayList(track._id)
+    const isInPlayList = this.isInPlayList(track._id)
     if (!isInPlayList) {
       this.clearPlayList()
       this.addToPlayList(track)
     }
-    this.playerService.play(track)
+
+    this.isPlaying = true
+    this.track = track
+    this.show()
+
     this.setState({
-      currentTrack: this.playerService.track,
-      showPlayer: this.playerService.visible,
-      isPlaying: this.playerService.isPlaying
+      currentTrack: this.track,
+      showPlayer: this.visible,
+      isPlaying: this.isPlaying
     })
   }
 
   pause = () => {
-    this.playerService.pause()
+    this.isPlaying = false
     this.setState({
-      isPlaying: this.playerService.isPlaying
+      isPlaying: this.isPlaying
     })
   }
 
   togglePlay = () => {
-    this.playerService.togglePlay()
-    const isPlaying = this.playerService.isPlaying
+    this.isPlaying = !this.isPlaying
     this.setState({
-      isPlaying
+      isPlaying: this.isPlaying
     })
-    return isPlaying
+    return this.isPlaying
   }
 
   hide = () => {
-    this.playerService.hide()
+    this.hide()
     this.setState({
-      showPlayer: this.playerService.visible,
-      isPlaying: this.playerService.isPlaying
+      showPlayer: this.visible,
+      isPlaying: this.isPlaying
     })
   }
 
+  addingToPlayList(track) {
+    if (track._id !== this.track._id && !this.isInPlayList(track._id)) {
+      this.playList.push(track)
+      return true
+    }
+    return false
+  }
+
+  addToPlayList = (track) => {
+    const isInPlayList = this.addingToPlayList(track)
+    if (!this.isPlaying && this.playList.length === 1) {
+      this.play(track)
+    }
+    this.setState({ playlist: this.playList })
+    return isInPlayList
+  }
+
+  removeFromPlayList = (track) => {
+    const index = this.getPlayListIndex(track._id)
+    if (index === -1) {
+      return false
+    }
+    this.playList.splice(index, 1)
+    this.setState({ playlist: this.playList })
+    return true
+  }
+
+  isInPlayList = (_id) => {
+    if (this.getPlayListIndex(_id) === -1) {
+      return false
+    }
+    return true
+  }
+
+  getPlayListIndex(_id) {
+    return findIndex(this.playList, { _id })
+  }
+
+  getNextTrack() {
+    const playListIndex = this.getPlayListIndex(this.track._id)
+    if (this.playList.length > playListIndex + 1) {
+      return this.playList[playListIndex + 1]
+    }
+    return null
+  }
+
   playNextTrack = () => {
-    const nextTrack = this.playerService.getNextTrack()
+    const nextTrack = this.getNextTrack()
     if (nextTrack) {
       this.play(nextTrack)
     } else {
@@ -58,28 +118,8 @@ export default class PlayerProvider {
   }
 
   clearPlayList = () => {
-    this.playerService.clearPlayList()
-    this.setState({ playlist: this.playerService.playList })
-  }
-
-  addToPlayList = (track) => {
-    const isInPlayList = this.playerService.addToPlayList(track)
-    if (
-      !this.playerService.isPlaying &&
-      this.playerService.playList.length === 1
-    ) {
-      this.play(track)
-    }
-    this.setState({ playlist: this.playerService.playList })
-    return isInPlayList
-  }
-
-  removeFromPlayList = (track) => {
-    this.setState({ playlist: this.playerService.playList })
-    return this.playerService.removeFromPlayList(track)
-  }
-
-  isInPlayList = (_id) => {
-    return this.playerService.isInPlayList(_id)
+    this.playListIndex = -1
+    this.playList = []
+    this.setState({ playlist: this.playList })
   }
 }
